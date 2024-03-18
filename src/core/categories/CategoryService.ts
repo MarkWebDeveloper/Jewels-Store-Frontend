4c61759058a535e6339a722a6747d5ca80cc61f5
Mark Kil <markkilcanarias@gmail.com>
2024-03-18 19:54:25 +0100

    category model and service added

---
+++ src/core/categories/CategoryService.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import type { ICategory } from "./ICategory";
+
+export default class CategoryService {
+
+    private uri = import.meta.env.VITE_APP_API_CategoryS
+
+    async get(): Promise<ICategory[]> {
+        try {
+            const response = await axios.get(this.uri)
+            const data: ICategory[] = await response.data
+            return data
+        } catch (error) {
+            throw new Error('Error with API calling: ' + error)
+        }
+    }
+
+}
