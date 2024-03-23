2fa8963d82da3ba6aad0cebd25d9a3c12113b577
Mark Kil <markkilcanarias@gmail.com>
2024-03-23 10:39:22 +0100

    new product button is created and working

---
+++ src/components/admin-dashboard/CreateProductForm.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="form-background">
+        <form action="submitForm">
+            <h2>Hello world</h2>
+        </form>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.form-background {
+    width: 100%;
+    height: 100%;
+    background-color: rgba($color: #000000, $alpha: 0.2);
+    z-index: 99;
+}
+</style>
--- src/views/AdminDashboard.vue
+++ src/views/AdminDashboard.vue
@@ -2,25 +2,40 @@
 import HeaderNav from '@/components/header/HeaderNav.vue';
 import Product from '@/components/admin-dashboard/Product.vue';
 import { useProductsStore } from '@/stores/productsStore';
+import { ref } from 'vue';
+import CreateProductForm from '@/components/admin-dashboard/CreateProductForm.vue';
 
 const productsStore = useProductsStore()
+
+const formIsOpened = ref<Boolean>(false)
+
+const openCloseForm = () => {
+  formIsOpened.value = !formIsOpened.value
+}
 </script>
 
 <template>
   <HeaderNav />
   <main>
     <h1 class="products-title">Products</h1>
+    <v-btn class="new-product-button rounded-lg" @click.prevent="openCloseForm()"><v-icon icon="mdi-plus" start></v-icon>New Product</v-btn>
+    <CreateProductForm v-if="formIsOpened"/>
     <div class="products-container">
-      <Product v-for="product in productsStore.products" v-if="productsStore.isLoaded" :product="product"/>
+      <Product v-for="product in productsStore.products" v-if="productsStore.isLoaded" :product="product" />
     </div>
   </main>
 </template>
 
 <style lang="scss" scoped>
+.new-product-button {
+  border: 0.1rem solid black;
+  margin-left: 2.5%;
+  margin-bottom: 2%;
+}
 .products-title {
   font-family: "Alex Brush", cursive;
   text-align: center;
-  margin: 1.5rem;
+  margin: 1rem;
   font-size: 2rem;
 }
 
