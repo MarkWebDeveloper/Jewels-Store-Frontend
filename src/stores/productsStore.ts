a45b38b46e00e847dd77cf1fa7715ea312fc3b83
Mark Kil <markkilcanarias@gmail.com>
2024-03-24 10:18:49 +0100

    first update commit

--- src/stores/productsStore.ts
+++ src/stores/productsStore.ts
@@ -1,4 +1,3 @@
-import { ref, computed, reactive } from "vue";
 import { defineStore } from "pinia";
 import ProductService from "@/core/products/ProductService";
 import type { IProduct } from "@/core/products/IProduct";
@@ -11,6 +10,7 @@ export const useProductsStore = defineStore('products', {
       isLoaded: false as boolean
     }
   },
+
   actions: {
     async getAllProducts(this: any): Promise<IProduct[]> {
       const service = new ProductService();
@@ -22,44 +22,19 @@ export const useProductsStore = defineStore('products', {
       console.log(this.products);
       return this.products;
     },
+
     async saveProduct(product: IProductDTO): Promise<void> {
       const service = new ProductService();
       await service.post(product);
     },
+
     async deleteProduct(id: number): Promise<void> {
       const service = new ProductService();
       await service.delete(id);
     },
+    
     deleteProductFromArray(id: number): void {
       this.products.splice(id, 1)
     }
   },
 })
-
-// export const useProductsStore = defineStore("products", () => {
-//   let products = reactive<IProduct[]>([]);
-//   const isLoaded = ref<Boolean>(false);
-
-//   async function getAllProducts(this: any): Promise<IProduct[]> {
-//     const service = new ProductService();
-//     if (this.isLoaded.value == true) {
-//       this.isLoaded.value = false
-//     }
-//     this.products = await service.get();
-//     isLoaded.value = true;
-//     console.log(this.products);
-//     return products;
-//   }
-
-//   async function saveProduct(product: IProductDTO): Promise<void> {
-//     const service = new ProductService();
-//     await service.post(product);
-//   }
-
-//   async function deleteProduct(id: number): Promise<void> {
-//     const service = new ProductService();
-//     await service.delete(id);
-//   }
-
-//   return { products, isLoaded, getAllProducts, saveProduct, deleteProduct };
-// });
