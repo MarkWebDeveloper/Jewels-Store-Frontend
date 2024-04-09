2568800afd2e167b2c3c8c791413de4d8601c0ef
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-04-09 18:23:01 +0200

    stripe public key is now hidden behind env variable

--- src/core/payments/stripe/stripe.ts
+++ src/core/payments/stripe/stripe.ts
@@ -1,5 +1,7 @@
 // stripe.js
 import { loadStripe } from '@stripe/stripe-js';
 
-const stripePromise = loadStripe('pk_test_51OzIVEFcCqnBf4sDhfmYifOmJeJhb8AXsX457SwIgraLI8OzL92By1zHzJss0rvWSCoI6d7daaZXqKDjbXcoKfEf00uzM47z2F');
+const stripePK: string = import.meta.env.VITE_APP_STRIPE_PK
+
+const stripePromise = loadStripe(stripePK);
 export default stripePromise;
