dcd2bcd20e960712adf477fd68ad8c67520f2d09
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-06-24 17:50:52 +0200

    login form design ready

--- src/components/global/LoginForm.vue
+++ src/components/global/LoginForm.vue
@@ -5,17 +5,20 @@
 <template>
     <div class="login-background">
         <div class="login-form-container">
-            <img class="login-flowers-frame" src="../../../public/images/pictures/frame-6790657_1920.png" alt="">
+            <img class="login-flowers-frame" src="/images/pictures/frame-6790657_1920.png" alt="">
             <h2>Login</h2>
             <label for="email-input">Email</label>
             <input class="email-input" type="email" name="email-input" id="email-input">
             <label for="password-input">Password</label>
             <input class="password-input" type="password" name="password-input" id="password-input">
+            <button class="login-button" id="default-login-button">Login</button>
             <h3>or</h3>
-            <button class="google-login-button" id="google-login-button">
-                <p>Login with</p>
-                <img class="google-icon" id="google-icon" src="../../../public/images/logos/icons8-google.svg" alt="">
+            <button class="login-button" id="google-login-button">
+                <p class="login-text">Login with</p>
+                <img class="google-icon" id="google-icon" src="/images/logos/icons8-google.svg" alt="">
             </button>
+            <p class="small-text">Don't have an account?</p>
+            <a href="#"><p class="register-text">Register now</p></a>
         </div>
     </div>
 </template>
@@ -64,7 +67,7 @@ input {
 }
 .login-flowers-frame {
     position: absolute;
-    top: 26%;
+    top: 19%;
     left: 0%;
     right: 0%;
     margin-left: auto;
@@ -72,25 +75,39 @@ input {
     width: 90%;
 }
 h3 {
-    margin-top: 2.5rem;
-    margin-bottom: 1rem;
+    margin: 0.3rem auto 0.3rem auto;
     font-family: "Alex Brush", cursive;
     font-size: 1.5rem;
 }
 button {
-    border-radius: 10px;
+    border-radius: 5px;
     border: black solid 1px;
     padding: 0.3rem 1rem 0.3rem 1rem;
     box-shadow: 2px 4px 3px lightgray;
+    width: 10rem;
+    min-height: 2.5rem;
+    font-family: "Aleo", serif;
 }
-p {
+#default-login-button {
+    margin-top: 3rem;
+}
+.login-text {
     font-family: "Aleo", serif;
     display: inline-block;
     margin-right: 0.5rem;
 }
-
 .google-icon {
     width: 2rem;
     display: inline;
+}
+.small-text {
+    font-size: 0.8rem;
+    margin: 1rem 0 0.5rem 0;
+}
+.register-text {
+    font-family: "Aleo", serif;
+    color: black;
+    text-decoration: underline;
+    cursor: pointer;
 }
 </style>
