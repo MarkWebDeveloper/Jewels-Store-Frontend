8f85cff8cb5c58c4a0650309b438fd52427ffdac
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-06-28 11:33:51 +0200

    login call structure and DTOs are prepared

---
+++ src/core/auth/ITokenDTO.ts
@@ -0,0 +1,5 @@
+export interface ITokenDTO {
+    userId: number,
+    accessToken: string,
+    refreshToken: string
+}
---
+++ src/core/auth/LoginService.ts
@@ -0,0 +1,28 @@
+import axios, { type AxiosRequestConfig } from "axios";
+import type { ILoginDTO } from "./ILoginDTO";
+import type { ITokenDTO } from "./ITokenDTO";
+
+export default class LoginService {
+
+    private uri: string = import.meta.env.VITE_APP_API_LOGIN
+
+    async post(loginDTO: ILoginDTO): Promise<ITokenDTO> {
+
+        let config: AxiosRequestConfig = {
+            maxBodyLength: Infinity,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        try {
+            const response = await axios.post(this.uri, loginDTO, config)
+            const status = response.status
+            console.log(status);
+            console.log(response.data)
+            return response.data            
+        } catch (error) {
+            throw new Error('Error with API calling: ' + error)
+        }
+    }
+}
