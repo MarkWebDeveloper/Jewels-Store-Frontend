453563f3f4b5242440b5e9cf3ce819cafd8812e1
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-07-05 13:57:47 +0200

    access token now refreshes automatically if the request failes

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@ import { useAlertsStore } from './stores/alertsStore';
 import { useImagesStore } from './stores/imagesStore';
 import { useHeaderStore } from './stores/headerStore';
 import { useLoginStore } from './stores/loginStore';
+import axios, { type AxiosRequestConfig } from 'axios';
+import type { IRefreshTokenDTO } from './core/auth/IRefreshTokenDTO';
 
 const productsStore = useProductsStore()
 const categoriesStore = useCategoriesStore()
@@ -18,6 +20,45 @@ productsStore.getAllProducts()
 categoriesStore.getAllCategories()
 // const gettingProducts = async () => { await productsStore.getAllProducts() }
 // gettingProducts()
+
+axios.interceptors.response.use(
+  response => response,
+  async error => {
+    const originalRequest = error.config;
+    // Check if the error is unauthorized
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      // Create refresh token DTO
+      let refreshToken: string = localStorage.getItem("refreshToken")!
+      let refreshTokenDTO: IRefreshTokenDTO = {
+        refreshToken: ""
+      }
+      refreshTokenDTO.refreshToken = refreshToken
+      let config: AxiosRequestConfig = {
+            maxBodyLength: Infinity,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+      try {
+        const response = await axios.post('http://localhost:8080/api/v1/all/token', refreshTokenDTO, config);
+        const { accessToken } = response.data;
+        // Set the new access token in your state
+        localStorage.setItem("userId", String(response.data.userId))
+        localStorage.setItem("accessToken", response.data.accessToken)
+        localStorage.setItem("refreshToken", response.data.refreshToken)
+        // Then retry the original request
+        originalRequest.headers['Authorization'] = `Bearer ${accessToken}`;
+        return axios(originalRequest);
+      } catch (err) {
+        // Handle error, e.g., logout user
+        console.error(err);
+        return Promise.reject(err);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 </script>
 
 <template>
